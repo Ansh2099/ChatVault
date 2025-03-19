b1dfdc5ca79360cd0186210f49f0df4d9a64ab9e
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-19 18:10:03 +0530

    Added a customized Frontend for the application

--- ChatVault_Frontend/src/environments/environment.prod.ts
+++ ChatVault_Frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   keycloakUrl: 'YOUR_DEPLOYED_KEYCLOAK_URL',
   keycloakRealm: 'ChatVault',
-  keycloakClientId: 'whatsapp-clone-app',
+  keycloakClientId: 'chatvault-app',
   appUrl: 'YOUR_DEPLOYED_FRONTEND_URL'
-}; 
+};
