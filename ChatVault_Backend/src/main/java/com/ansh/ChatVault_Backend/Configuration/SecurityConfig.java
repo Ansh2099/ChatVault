2d7f76f3853e9ceca30f5ca4aa0cdc1aca6bddd4
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-26 19:08:18 +0530

    Minor Changes for cron-job

--- ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Configuration/SecurityConfig.java
+++ ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Configuration/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                                         "/swagger-ui/**",
                                         "/webjars/**",
                                         "/swagger-ui.html",
-                                        "/ws/**")
+                                        "/ws/**",
+                                        "/health")
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
