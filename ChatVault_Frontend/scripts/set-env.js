5f2040f9c830f8ce60759b9cfd893c5ea2b430b1
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-29 15:02:45 +0530

    Fixed environment script

--- ChatVault_Frontend/scripts/set-env.js
+++ ChatVault_Frontend/scripts/set-env.js
@@ -1,30 +1,9 @@
-import { writeFileSync } from 'fs';
-
+const { writeFileSync } = require('fs');
 require('dotenv').config();
 
 const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
-  production: true,
-  keycloakUrl: '${process.env.KEYCLOAK_URL}',
-  keycloakRealm: 'ChatVault',
-  keycloakClientId: 'chatvault-app',
-  appUrl: '${process.env.APP_URL}',
-  apiUrl: '${process.env.API_URL}'
-};
-`;
-
-writeFileSync(targetPath, envConfigFile);
-console.log(`Environment variables written to ${targetPath}`);
-const fs = require('fs');
-
-const { writeFileSync } = fs;
-
-require('dotenv').config();
-
-const prodTargetPath = './src/environments/environment.prod.ts';
-
-const prodEnvConfigFile = `export const environment = {
   production: true,
   keycloakUrl: '${process.env.KEYCLOAK_URL}',
   keycloakRealm: 'ChatVault',
