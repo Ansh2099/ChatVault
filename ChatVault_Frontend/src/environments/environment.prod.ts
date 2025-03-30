dfc428ad4c546b81b2eeeac4ad2cbb7d7c5b7ee1
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-30 12:24:51 +0530

    Fixed Netlify compatibility issues

--- ChatVault_Frontend/src/environments/environment.prod.ts
+++ ChatVault_Frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  keycloakUrl: 'YOUR_KEYCLOAK_URL', // Will be replaced dynamically in the build
+  keycloakUrl: 'undefined',
   keycloakRealm: 'ChatVault',
   keycloakClientId: 'chatvault-app',
-  appUrl: 'YOUR_FRONTEND_URL', // Will be replaced dynamically in the build
-  apiUrl: 'YOUR_API_URL' // Will be replaced dynamically in the build
+  appUrl: 'undefined',
+  apiUrl: 'undefined'
 };
