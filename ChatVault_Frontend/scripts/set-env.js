f70c5efee14458d96ab1b0ff1ae9d0da0f767a1c
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-03-31 14:47:34 +0530

    Compatibility issues resolved

--- ChatVault_Frontend/scripts/set-env.js
+++ ChatVault_Frontend/scripts/set-env.js
@@ -5,11 +5,11 @@ const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
   production: true,
-  keycloakUrl: '${process.env.KEYCLOAK_URL}',
+  keycloakUrl: '${process.env.YOUR_KEYCLOAK_URL}',
   keycloakRealm: 'ChatVault',
   keycloakClientId: 'chatvault-app',
-  appUrl: '${process.env.APP_URL}',
-  apiUrl: '${process.env.API_URL}'
+  appUrl: '${process.env.YOUR_FRONTEND_URL}',
+  apiUrl: '${process.env.YOUR_API_URL}'
 };
 `;
 
