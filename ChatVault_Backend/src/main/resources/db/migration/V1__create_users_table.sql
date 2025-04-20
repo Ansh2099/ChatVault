63c27dc8ff43f39243392caabbfba03f6d631c7b
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-20 15:48:40 +0530

    Created a sql file for flyway, for reliable table creation.

---
+++ ChatVault_Backend/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id VARCHAR(255) PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    last_seen TIMESTAMP,
+    created_date TIMESTAMP,
+    last_modified_date TIMESTAMP
+);
