8a3e4f9223dfcd07d419a391091509598f84337c
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-20 16:13:30 +0530

    Created more sql files fors flyway, for reliable table creation.

--- ChatVault_Backend/src/main/resources/db/migration/V2__create_chat_table.sql
+++ ChatVault_Backend/src/main/resources/db/migration/V2__create_chat_table.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 CREATE TABLE chat (
     id VARCHAR(255) PRIMARY KEY,
     sender_id VARCHAR(255) NOT NULL,
@@ -6,4 +7,14 @@ CREATE TABLE chat (
     last_modified_date TIMESTAMP,
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (recipient_id) REFERENCES users(id)
+=======
+CREATE TABLE chat (
+    id VARCHAR(255) PRIMARY KEY,
+    sender_id VARCHAR(255) NOT NULL,
+    recipient_id VARCHAR(255) NOT NULL,
+    created_date TIMESTAMP,
+    last_modified_date TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (recipient_id) REFERENCES users(id)
+>>>>>>> 0e1f62a (Created more sql files fors flyway, for reliable table creation.)
 );
