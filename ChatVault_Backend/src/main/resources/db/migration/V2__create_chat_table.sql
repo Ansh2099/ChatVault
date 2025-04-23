2be04a42063c09123000d84dd918f0856aec620b
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-23 12:41:52 +0530

    Minor version control endpoint changes

--- ChatVault_Backend/src/main/resources/db/migration/V2__create_chat_table.sql
+++ ChatVault_Backend/src/main/resources/db/migration/V2__create_chat_table.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE chat (
     id VARCHAR(255) PRIMARY KEY,
     sender_id VARCHAR(255) NOT NULL,
@@ -7,14 +6,5 @@ CREATE TABLE chat (
     last_modified_date TIMESTAMP,
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (recipient_id) REFERENCES users(id)
-=======
-CREATE TABLE chat (
-    id VARCHAR(255) PRIMARY KEY,
-    sender_id VARCHAR(255) NOT NULL,
-    recipient_id VARCHAR(255) NOT NULL,
-    created_date TIMESTAMP,
-    last_modified_date TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (recipient_id) REFERENCES users(id)
->>>>>>> 0e1f62a (Created more sql files fors flyway, for reliable table creation.)
 );
+
--- ChatVault_Backend/src/main/resources/db/migration/V3__create_messages_table.sql
+++ ChatVault_Backend/src/main/resources/db/migration/V3__create_messages_table.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE SEQUENCE msg_seq START 1 INCREMENT 1;
 
 CREATE TABLE messages (
@@ -15,22 +14,5 @@ CREATE TABLE messages (
     FOREIGN KEY (chat_id) REFERENCES chat(id),
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (receiver_id) REFERENCES users(id)
-=======
-CREATE SEQUENCE msg_seq START 1 INCREMENT 1;
-
-CREATE TABLE messages (
-    id BIGINT PRIMARY KEY DEFAULT nextval('msg_seq'),
-    content TEXT,
-    state VARCHAR(50),
-    type VARCHAR(50),
-    chat_id VARCHAR(255) NOT NULL,
-    sender_id VARCHAR(255) NOT NULL,
-    receiver_id VARCHAR(255) NOT NULL,
-    media_file_path VARCHAR(255),
-    created_date TIMESTAMP,
-    last_modified_date TIMESTAMP,
-    FOREIGN KEY (chat_id) REFERENCES chat(id),
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
->>>>>>> 0e1f62a (Created more sql files fors flyway, for reliable table creation.)
 );
+
