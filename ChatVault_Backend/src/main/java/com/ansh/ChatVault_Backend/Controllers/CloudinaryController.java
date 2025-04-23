2817c9959b8cd65a37ac70c94bbab5d97caea72e
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-23 12:24:18 +0530

    Minor version control endpoint changes

--- ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Controllers/CloudinaryController.java
+++ ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Controllers/CloudinaryController.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/cloudinary")
+@RequestMapping("/api/v1/cloudinary")
 @RequiredArgsConstructor
 @Tag(name = "Cloudinary_Controller")
 public class CloudinaryController {
