5bbfb18128af2e1583ead3a2249e8deaea61d335
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-04-24 09:40:47 +0530

    Minor changes to the custom exceptions

--- ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Exceptions/CustomExceptions/UserNotFoundException.java
+++ ChatVault_Backend/src/main/java/com/ansh/ChatVault_Backend/Exceptions/CustomExceptions/UserNotFoundException.java
@@ -1,4 +1,5 @@
 package com.ansh.ChatVault_Backend.Exceptions.CustomExceptions;
 
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message){super(message);}
 }
